59b419c8c47526feaacaddf737a60249c01b147f
cameronMG-J <cameron.mcgrath.johnston@gmail.com>
2025-04-15 15:56:47 +0100

    added modularity to layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,26 @@ import {Tabs} from 'expo-router';
 import {images} from "@/constants/images";
 import { icons } from '@/constants/icons';
 
+const TabIcon = ({focused, icon, title}) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"  
+        >
+          <Image source={icon} tintColor="#151312" className="size-5"/>
+          <Text className="text-secondary text-base font-semibold ml-2">{title}</Text>
+      </ImageBackground>
+    )
+  }
+
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
+    </View>
+  )
+}
+
 const _Layout = () => {
   return (
     <Tabs>
@@ -14,15 +34,11 @@ const _Layout = () => {
           title: 'Home', 
           headerShown: false,
           tabBarIcon: ({focused}) =>
-            <>
-              <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"  
-              >
-                  <Image source={icons.home} tintColor="#151312" className="size-5"/>
-                  <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
-              </ImageBackground>
-            </>
+            <TabIcon 
+              focused={focused}
+              icon={icons.home}
+              title="Home"
+            />
         }}
       />
 
@@ -31,7 +47,13 @@ const _Layout = () => {
         options=
         {{
           title: 'Saved', 
-          headerShown: false
+          headerShown: false,
+          tabBarIcon: ({focused}) =>
+            <TabIcon 
+              focused={focused}
+              icon={icons.save}
+              title="Saved"
+            />
         }}
       />
       
@@ -40,7 +62,13 @@ const _Layout = () => {
         options=
         {{
           title: 'Profile', 
-          headerShown: false
+          headerShown: false,
+          tabBarIcon: ({focused}) =>
+            <TabIcon 
+              focused={focused}
+              icon={icons.person}
+              title="Profile"
+            />
         }}
       />
 
@@ -49,7 +77,13 @@ const _Layout = () => {
         options=
         {{
           title: 'Search', 
-          headerShown: false
+          headerShown: false,
+          tabBarIcon: ({focused}) =>
+            <TabIcon 
+              focused={focused}
+              icon={icons.search}
+              title="Search"
+            />
         }}
       />
     </Tabs>
